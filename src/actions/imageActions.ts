13ddd478358a66958315bee0d9742801bdc7a3f2
Kimfucious <kim@kimford.me>
2023-01-27 17:29:11 +0700

    change image order

--- src/actions/imageActions.ts
+++ src/actions/imageActions.ts
@@ -1,9 +1,9 @@
-import image_1 from "../assets/images/bw.webp";
+import image_1 from "../assets/images/bm_3.webp";
 import image_2 from "../assets/images/pt.webp";
-import image_3 from "../assets/images/bm.webp";
-import image_4 from "../assets/images/sly_and_robbie.webp";
-import image_5 from "../assets/images/bm_3.webp";
-import image_6 from "../assets/images/jc.webp";
+import image_3 from "../assets/images/bw.webp";
+import image_4 from "../assets/images/jc.webp";
+import image_5 from "../assets/images/bm.webp";
+import image_6 from "../assets/images/sly_and_robbie.webp";
 import image_7 from "../assets/images/pt_2.webp";
 import image_8 from "../assets/images/bm_mj_pt.webp";
 import image_9 from "../assets/images/sd.webp";
@@ -12,11 +12,11 @@ import type { SliderImage } from "../types";
 export function loadImageData() {
     try {
         const images: SliderImage[] = [
-            { path: image_1, altText: "Bunny Wailer" },
+            { path: image_1, altText: "Bob Marley" },
             { path: image_2, altText: "Peter Tosh" },
-            { path: image_3, altText: "Bob Marley" },
-            { path: image_4, altText: "Sly & Robbie" },
-            { path: image_5, altText: "Bob Marley 3" },
+            { path: image_3, altText: "Bunny Wailer" },
+            { path: image_4, altText: "Jimmy Cliff" },
+            { path: image_5, altText: "Sly & Robbie" },
             { path: image_6, altText: "Jimmy Cliff" },
             { path: image_7, altText: "Peter Tosh" },
             {
@@ -67,28 +67,45 @@ function renderImage(
     sliderX: number
 ) {
     console.log(`Rendering ${description}...`);
-    const scaleFactor = getImageScale(ctx, description, image);
+    const cAspectRatio = ctx.canvas.width / ctx.canvas.height;
+    const iAspectRatio = image.width / image.height;
+    const scaleFactor = getImageScale(
+        cAspectRatio,
+        ctx,
+        description,
+        iAspectRatio,
+        image
+    );
+    let height = image.height * scaleFactor;
+    let width = image.width * scaleFactor;
+    if (image.height >= ctx.canvas.height || image.width >= ctx.canvas.height) {
+        height -= 50;
+        width -= 50;
+    }
     const x =
-        offset + sliderX + (ctx.canvas.width - image.width * scaleFactor) / 2;
-    const y = (ctx.canvas.height - image.height * scaleFactor) / 2;
+        // offset + sliderX + (ctx.canvas.width - image.width * scaleFactor) / 2;
+        offset + sliderX + (ctx.canvas.width - width) / 2;
+    const y = (ctx.canvas.height - height) / 2;
     ctx.drawImage(
         image,
         Math.round(x),
         Math.round(y),
-        Math.round(image.width * scaleFactor),
-        Math.round(image.height * scaleFactor)
+        Math.round(width),
+        Math.round(height)
     );
-    // https://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
-    // requestAnimationFrame(renderImage)
+    /* 
+    Not sure if I need this:
+    https://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+    */
 }
 
 function getImageScale(
+    cAspectRatio: number,
     ctx: CanvasRenderingContext2D,
     description: string,
+    iAspectRatio: number,
     image: HTMLImageElement
 ) {
-    const cAspectRatio = ctx.canvas.width / ctx.canvas.height;
-    const iAspectRatio = image.width / image.height;
     if (iAspectRatio >= cAspectRatio) {
         if (image.width > ctx.canvas.width) {
             return ctx.canvas.width / image.width;
