fbb66075df7faaa68e3d4b67d6451e99b68bcf02
Kimfucious <kim@kimford.me>
2023-02-03 08:41:39 +0700

    load valid image IDs from api to prevent 400 errs

--- src/api/ImageLoader/ImageLoader.ts
+++ src/api/ImageLoader/ImageLoader.ts
@@ -1,7 +1,11 @@
+import axios from "axios";
 import { HomeState } from "../../pages/home";
+import { ImageDataResponse } from "../../types";
 
 export default class ImageLoader {
     private _endpoint: string = "";
+    private _imageIds: number[] = [];
+    private _counter: number = 0;
 
     constructor(endpoint: string) {
         if (endpoint) {
@@ -9,7 +13,25 @@ export default class ImageLoader {
         }
     }
 
-    getRandomImageSource(startImage: number, idx: number): string {
+    async getImages() {
+        let page = 0;
+        console.log("%c🐕 Fetching IDs from image api", "color:cyan");
+        while (this._imageIds.length < 1000) {
+            const { data } = await axios(
+                `https://picsum.photos/v2/list?page=${page}&limit=100`
+            );
+            const ids = data.map((item: ImageDataResponse) => item.id);
+            this._imageIds = [...this._imageIds, ...ids];
+            console.log(
+                `%c🐕 Fetched ${this._imageIds.length} IDs so far...`,
+                "color:cyan"
+            );
+            page++;
+        }
+        this._imageIds = this._imageIds.slice(10);
+    }
+
+    getRandomImageSource(startPoint: number, idx: number): string {
         const widths = [320, 576, 640, 769, 1024];
         const width = widths[Math.floor(Math.random() * widths.length)];
         const aspectRatios = [1, 4 / 3, 16 / 9];
@@ -18,7 +40,9 @@ export default class ImageLoader {
         const height = Math.round(width / aspectRatio);
 
         const BASE_URL = this._endpoint;
-        const id = startImage + idx;
+        // const id = startPoint + idx;
+        const id = this._imageIds[startPoint + idx];
+        // const path = `id/${id}/${width}/${height}.webp`;
         const path =
             aspectRatio === 1
                 ? `id/${id}/${width}.webp`
@@ -26,38 +50,36 @@ export default class ImageLoader {
         return BASE_URL + "/" + path;
     }
 
-    getRandomImageSources(startImage: number, qty: number): string[] {
+    getRandomImageSources(startPoint: number, qty: number): string[] {
         const sources: string[] = [];
         for (let i = 0; i < qty; i++) {
-            const source = this.getRandomImageSource(startImage, i);
+            const source = this.getRandomImageSource(startPoint, i);
             sources.push(source);
         }
-        console.log("sources", sources);
         return sources;
     }
 
-    initImages(
+    async initImages(
         isLoading: React.MutableRefObject<boolean>,
         qty: number,
-        startImage: number,
         state: HomeState,
         setState: (s: HomeState) => void
-    ): void {
-        const sources = this.getRandomImageSources(startImage, qty);
+    ): Promise<void> {
+        await this.getImages();
+        const startPoint = Math.floor(this._imageIds.length / 2);
+        const sources = this.getRandomImageSources(startPoint, qty);
         this.loadImages(true, isLoading, qty, sources, state, setState);
     }
 
     loadMoreImages(
         isLoading: React.MutableRefObject<boolean>,
         qty: number,
-        startImage: number,
         state: HomeState,
         setState: (s: HomeState) => void
     ): void {
-        const sources = this.getRandomImageSources(
-            startImage + state.images.length,
-            qty
-        );
+        const mid = Math.floor(this._imageIds.length / 2);
+        const startPoint = mid + state.images.length;
+        const sources = this.getRandomImageSources(startPoint, qty);
         this.loadImages(false, isLoading, qty, sources, state, setState);
     }
 
@@ -76,15 +98,15 @@ export default class ImageLoader {
             const image = new Image();
             image.onload = function () {
                 loadCount++;
-                console.log("%cImage loaded:", "color:lime", image.src);
+                // console.log("%c🖼️ Image loaded:", "color:lime", image.src);
                 if (loadCount === qty) {
                     isInitialLoad
                         ? console.log(
-                              `%cAll ${sources.length} initial images have been loaded.`,
+                              `%c🖼️ All ${sources.length} initial images have been loaded.`,
                               "color:green"
                           )
                         : console.log(
-                              `%c${sources.length} more images have been loaded.`,
+                              `%c🖼️ ${sources.length} more images have been loaded.`,
                               "color:green"
                           );
                     /* 
